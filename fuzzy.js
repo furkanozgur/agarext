5faf7db22d8e93583ffdc4e05e0024baf5280747
fuzzy <fffurkanozgurrr@gmail.com>
2017-05-19 10:54:45 +0300

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -2,7 +2,6 @@ window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 var imlost = 25;
 function keydown(event) {
- // Triplesplit Z
     if (event.keyCode == 68) {
         ilikedick();
         setTimeout(ilikedick, imlost);

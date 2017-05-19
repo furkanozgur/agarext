f51e1e7dbda0475a5c44141bee6f3704c3aded26
fuzzy <fffurkanozgurrr@gmail.com>
2017-05-19 11:06:52 +0300

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -8,3 +8,7 @@ function keydown(event) {
         setTimeout(ilikedick, imlost*2);
     }
 }
+function ilikedick() {
+    $("body").trigger($.Event("keydown", { keyCode: 32}));
+    $("body").trigger($.Event("keyup", { keyCode: 32}));
+}

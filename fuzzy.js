fcc352defaf4b70b13b999e19617297dd80c05bf
fuzzy <fffurkanozgurrr@gmail.com>
2017-05-19 11:20:37 +0300

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -2,13 +2,14 @@ window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 var imlost = 25;
 function keydown(event) {
+     if (event.keyCode == 81 ) { //( ͡° ͜ʖ ͡°)
+        ilikedick();
+        setTimeout(ilikedick, imlost);
+        setTimeout(ilikedick, imlost*2);
+        setTimeout(ilikedick, imlost*3);
     if (event.keyCode == 68) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
     }
-}
-function ilikedick() {
-    $("body").trigger($.Event("keydown", { keyCode: 32}));
-    $("body").trigger($.Event("keyup", { keyCode: 32}));
 }

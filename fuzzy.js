05b4e85a8e243b1b447653600b933ea88362a196
fuzzy <fffurkanozgurrr@gmail.com>
2017-05-19 10:52:48 +0300

    Update and rename index.md to fuzzy.js

---
+++ fuzzy.js
@@ -0,0 +1,11 @@
+window.addEventListener('keydown', keydown);
+window.addEventListener('keyup', keyup);
+var imlost = 25;
+function keydown(event) {
+ // Triplesplit Z
+    if (event.keyCode == 68) {
+        ilikedick();
+        setTimeout(ilikedick, imlost);
+        setTimeout(ilikedick, imlost*2);
+    }
+}

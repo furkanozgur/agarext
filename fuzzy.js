be3862ababd44dced43d17f2e52d428f51466352
fuzzy <fffurkanozgurrr@gmail.com>
2017-05-19 11:27:34 +0300

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -2,11 +2,7 @@ window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
 var imlost = 25;
 function keydown(event) {
-     if (event.keyCode == 81 ) { //( ͡° ͜ʖ ͡°)
-        ilikedick();
-        setTimeout(ilikedick, imlost);
-        setTimeout(ilikedick, imlost*2);
-        setTimeout(ilikedick, imlost*3);
+     
     if (event.keyCode == 68) {
         ilikedick();
         setTimeout(ilikedick, imlost);
